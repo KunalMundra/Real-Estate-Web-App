52d39d6a36d67a5c7918448cffb7c3ee46ea7ece
KunalMundra <kunalmundra2@gmail.com>
2024-08-05 23:55:43 +0530

    feat: create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,32 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js"
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({ message: "API route is working" })
+}
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account"))
+    try {
+
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.password
+            }
+        }, { new: true })
+
+        const { password, ...restInfo } = updatedUser._doc
+        res.status(200).json(restInfo);
+
+    } catch (error) {
+        next(error)
+    }
 }
